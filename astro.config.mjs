23cdbd91785768af6e7b52547790143f208de783
alann-estrada <142933070+alann-estrada@users.noreply.github.com>
2024-06-13 15:33:40 -0600

    Finished project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: 'hybrid' // para que sea un sitio con cosas estáticas y dinámicas
+  output: 'hybrid'
+  // output: 'hybrid' // para que sea un sitio con cosas estáticas y dinámicas
   // output: 'server' // para que sea un sitio con cosas dinámicas
   // output: 'static' // para que sea un sitio con cosas estáticas
+  ,
+  adapter: node({
+    mode: "standalone"
+  })
 });
